c4790cb38e57eefecb867be802fad2d8297cdf40
Cassiano Leal <cassiano.leal@gamesys.co.uk>
2014-10-01 13:29:11 +0100

    Populate Project attributes on object creation

--- lib/cloudstack_resources/project.rb
+++ lib/cloudstack_resources/project.rb
@@ -2,22 +2,27 @@ class CloudstackResources::Project
 
   def initialize(properties)
     @conn = CloudstackResources::connection
-    @properties = properties
     populate(properties)
   end
 
   def populate(properties)
-    # Create attributes from the hash's keys
-  end
-
-  def self.all
-    projects = CloudstackResources::connection.list_projects
-    projects['project'].collect { |p| self.new(p) }
+    self.class.create_attributes(properties.keys)
+    properties.each do |p|
+      self.send("#{p.first}=", p.last)
+    end
   end
 
   def self.where(params = {})
     projects = CloudstackResources::connection.list_projects(params)
     projects['project'].collect { |p| self.new(p) }
+  end
+
+  def self.create_attributes(attributes)
+    attributes.each { |attribute| attr_accessor attribute }
+  end
+
+  class << self
+    alias_method :all, :where
   end
 
 end
